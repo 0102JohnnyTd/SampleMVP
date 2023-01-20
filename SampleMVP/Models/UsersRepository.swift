8a0888b2ea16e274f334886edd6ecc74afd2cbb4
0102johnny <rt2824.recruit.1022@gmail.com>
2023-01-20 17:48:50 +0900

    GitHubユーザーのリポジトリデータを格納するModelを定義

--- SampleMVP/Models/UsersRepository.swift
+++ SampleMVP/Models/UsersRepository.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+
+// GitHubユーザーのリポジトリデータを格納するModel
+struct UsersRepository {
+    // リポジトリ名
+    var name: String
+    // リポジトリの説明文
+    var description: String?
+    // 言語
+    var language: String
+}
