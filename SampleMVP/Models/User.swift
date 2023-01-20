593562f897b0e5b7765d7cc555727c9161f78006
0102johnny <rt2824.recruit.1022@gmail.com>
2023-01-20 10:33:18 +0900

    GitHubユーザーのデータを格納するModelを定義

---
+++ SampleMVP/Models/User.swift
@@ -0,0 +1,22 @@
+//
+//  User.swift
+//  SampleMVP
+//
+//  Created by Johnny Toda on 2023/01/19.
+//
+
+import Foundation
+
+// GitHubユーザーのデータを格納するModel
+struct User {
+    // ユーザー名
+    var name: String
+    // ユーザーのプロフィール画像
+    var imageURL: String
+
+    // プロパティに対する代替keyを指定
+    enum CodingKeys: String, CodingKey {
+        case name = "login"
+        case imageURL = "avatar_url"
+    }
+}
---
+++ SampleMVP/Models/UsersRepository.swift
@@ -0,0 +1,8 @@
+//
+//  UsersRepository.swift
+//  SampleMVP
+//
+//  Created by Johnny Toda on 2023/01/19.
+//
+
+import Foundation
