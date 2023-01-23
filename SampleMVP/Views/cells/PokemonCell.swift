775b1db8535be9141643dd2f9c33a19e36589f52
0102johnny <rt2824.recruit.1022@gmail.com>
2023-01-24 07:47:54 +0900

    XIBファイルのCellを作成し、クラスファイルにXIB上のオブジェクトを@IBOutletで接続

---
+++ SampleMVP/Views/cells/PokemonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PokemonCell.swift
+//  SampleMVP
+//
+//  Created by Johnny Toda on 2023/01/24.
+//
+
+import UIKit
+
+class PokemonCell: UITableViewCell {
+    @IBOutlet private weak var iconView: UIImageView!
+    @IBOutlet private weak var idLabel: UILabel!
+    @IBOutlet private weak var nameLabel: UILabel!
+}
