ebdbee481e501b68dfd49364ce89e4dddaad189d
0102johnny <rt2824.recruit.1022@gmail.com>
2023-01-24 11:42:20 +0900

    PokemonCellのオブジェクトに値を入力する処理を実装

--- SampleMVP/Views/cells/PokemonCell.swift
+++ SampleMVP/Views/cells/PokemonCell.swift
@@ -6,9 +6,23 @@
 //
 
 import UIKit
+import Kingfisher
 
-class PokemonCell: UITableViewCell {
+final class PokemonCell: UITableViewCell {
     @IBOutlet private weak var iconView: UIImageView!
     @IBOutlet private weak var idLabel: UILabel!
     @IBOutlet private weak var nameLabel: UILabel!
+
+    // 2パターン用意してみた。
+    func configure(pokemon: Pokemon) {
+        iconView.kf.setImage(with: URL(string: pokemon.sprites.frontImage))
+        idLabel.text = String(pokemon.id)
+        nameLabel.text = pokemon.name
+    }
+
+//    func configure(imageURL: String, id: String, name: String) {
+//        iconView.kf.setImage(with: URL(string: imageURL))
+//        idLabel.text = id
+//        nameLabel.text = name
+//    }
 }
